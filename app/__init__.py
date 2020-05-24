47b301c060fd9f4ab98127581076122f86d4e951
krisfloc <kristian9818@hotmail.com>
2020-05-24 19:19:13 +0200

    Connect to heroku database v3

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from flask_migrate import Migrate
 #}
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lgzazgjiscblnh:b817d88f94aefcf2c4b892a5aea7f8b996b01550225c500423e7fabde63a18ca@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d3ntkg8sda8oq1'
 
 app.config.from_pyfile('config.py')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
